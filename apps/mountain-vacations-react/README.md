92bf367821067f39c0023e8cb0d8fd22a3fa7787
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-09-06 20:04:06 -0400

    Added mountain-vacations-react

---
+++ apps/mountain-vacations-react/README.md
@@ -0,0 +1,4 @@
+# Mountain Vacations React
+
+This is a [React](https://reactjs.org/) app bootstrapped with
+[Code Shaper](https://code-shaper.dev).
---
+++ apps/mountain-vacations-react/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="header">
+      <span className="header__title">Mountain Vacations React</span>
+    </header>
+  );
+}
